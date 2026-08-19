73b9a649478498122a733a269334f066ad9f9297
Zack Turco <2rscrw8cmn@privaterelay.appleid.com>
2026-08-18 23:36:31 -0400

    Remove approved award visual-study route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
-import { AwardBlockVisualStudyRoute } from "./features/crew/awardBlockVisualStudyRoute";
 import { GettingStartedPage } from "./features/help/GettingStartedPage";
 import "@fontsource/space-mono/400.css";
 import "@fontsource/space-mono/700.css";
@@ -17,15 +16,7 @@ if (!rootElement) {
 }
 
 const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
-const visualStudy = new URLSearchParams(window.location.search).get("visual");
-const content =
-  visualStudy === "award-blocks"
-    ? <AwardBlockVisualStudyRoute />
-    : pathname === "/getting-started"
-      ? <GettingStartedPage />
-      : pathname === "/award-blocks"
-        ? <AwardBlockVisualStudyRoute />
-        : <App />;
+const content = pathname === "/getting-started" ? <GettingStartedPage /> : <App />;
 
 createRoot(rootElement).render(
   <StrictMode>
