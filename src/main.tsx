6e31284066339613b194f10f4b0012a2ee17d147
Zack Turco <2rscrw8cmn@privaterelay.appleid.com>
2026-08-18 22:16:10 -0400

    Expose award block visual study at /award-blocks

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import { AppErrorBoundary } from "./app/AppErrorBoundary";
+import { AwardBlockVisualStudyRoute } from "./features/crew/awardBlockVisualStudyRoute";
 import { GettingStartedPage } from "./features/help/GettingStartedPage";
 import "@fontsource/space-mono/400.css";
 import "@fontsource/space-mono/700.css";
@@ -16,7 +17,12 @@ if (!rootElement) {
 }
 
 const pathname = window.location.pathname.replace(/\/+$/, "") || "/";
-const content = pathname === "/getting-started" ? <GettingStartedPage /> : <App />;
+const content =
+  pathname === "/getting-started"
+    ? <GettingStartedPage />
+    : pathname === "/award-blocks"
+      ? <AwardBlockVisualStudyRoute />
+      : <App />;
 
 createRoot(rootElement).render(
   <StrictMode>
